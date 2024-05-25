c39c6492ccbf9c99b5b6719230638e30965ea09e
Maicol Salazar <stivsal12@gmail.com>
2024-05-25 11:23:45 -0500

    despliegue

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // Nombre del archivo JSON donde se encuentra tu base de datos
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
-const port = process.env.PORT || 3000
-server.listen(port, () => {
-  console.log('JSON Server is running on port', port)
-})
+const PORT = process.env.PORT || 3001; // Puerto en el que se ejecutará el servidor
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
+
